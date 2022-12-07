b38e0d85cfe2c2cae0765a0bd0206bf7b9acfc6e
Everett <53265846+Dtet@users.noreply.github.com>
2022-12-07 13:52:04 -0400

    primer commit
    estructura del proyecto

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,22 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { ListComponent } from './modules/facultative-list/pages/list/list.component';
+import { FormComponent } from './modules/facultative-form/pages/form/form.component';
+import { HeaderComponent } from './shared/components/header/header.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ListComponent,
+    FormComponent,
+    HeaderComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/modules/facultative-form/facultative-form.module.ts
@@ -0,0 +1,14 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { FacultativeFormRoutingModule } from './facultative-form-routing.module';
+
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule,
+    FacultativeFormRoutingModule
+  ]
+})
+export class FacultativeFormModule { }
